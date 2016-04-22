f9ef47befdc117fd56dd87979a3468b1fbef1397
Alan Colver <accolver@gmail.com>
2016-04-21 23:26:28 -0600

    Replace missing data by sampling existing data

--- clean_data.R
+++ clean_data.R
@@ -8,8 +8,24 @@ write.csv <- function(ob, filename) {
   write.table(ob, filename, quote = FALSE, sep = ",", row.names = FALSE)
 }
 
+# Remove columns (cols) from a data frame (df)
 removeColumns <- function (df, cols) {
   df[, -which(names(df) %in% cols)]
+}
+
+# Replace all fields in `data` matching `value` with sample fields from `data`
+replaceWithSampling <- function (value, data) {
+  nc <- ncol(data)
+  for (i in 1:nc) {
+    col <- data[, i]
+    sampleData <- col[col != value]
+    for (j in 1:length(col)) {
+      if (is.na(col[j]) || col[j] == value) {
+        data[, i][j] <- sample(sampleData, 1)
+      }
+    }
+  }
+  data
 }
 
 is999999default <- function (value) {
@@ -93,4 +109,43 @@ redundantColumns <- c('BTUEL')
 data.energy <- removeColumns(data.energy, redundantColumns)
 data.energy <- removeColumns(data.energy, insufficientColumns)
 
-# write.csv(data.energy, 'cleaned_data.csv')
+
+################
+# FILL THE DATA
+################
+
+
+set.seed(333) # Reproducible results
+
+cols <- colnames(data.energy)
+colsWithDefaults <- sapply(cols, is999999default)
+if (any(colsWithDefaults)) {
+  sub.data.energy <- data.energy[colsWithDefaults]
+  sub.data.energy <- replaceWithSampling(9999999, sub.data.energy)
+  sub.data.energy <- replaceWithSampling(999999, sub.data.energy)
+  data.energy <- data.energy[!colsWithDefaults]
+  data.energy <- cbind(data.energy, sub.data.energy)
+  # data.energy <- merge(x = data.energy, y = sub.data.energy)
+}
+
+cols <- colnames(data.energy)
+colsWithDefaults <- sapply(cols, is99default)
+if (any(colsWithDefaults)) {
+  sub.data.energy <- data.energy[colsWithDefaults]
+  sub.data.energy <- replaceWithSampling(99, sub.data.energy)
+  data.energy <- data.energy[!colsWithDefaults]
+  data.energy <- cbind(data.energy, sub.data.energy)
+}
+
+cols <- colnames(data.energy)
+colsWithDefaults <- sapply(cols, is9default)
+if (any(colsWithDefaults)) {
+  sub.data.energy <- data.energy[colsWithDefaults]
+  sub.data.energy <- replaceWithSampling(9, sub.data.energy)
+  data.energy <- data.energy[!colsWithDefaults]
+  data.energy <- cbind(data.energy, sub.data.energy)
+}
+
+
+# Write out the file
+write.csv(data.energy, 'cleaned_data.csv')
